7350705e4cf0724c14444206471d26223e657188
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-22 14:46:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,8 @@
-# dog.rb
+class Dog
+  def bark
+    puts "Woof!"
+  end
+  def sit
+    puts "The Dog is sitting"
+  end
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,8 @@
-# person.rb
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+  end
+end
